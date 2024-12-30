ea7625651cfb46e0e6edd90289b1cb5763016ebc
Matheus Farias <mf0560272@gmail.com>
2024-12-30 16:52:54 -0300

    Primeiro código em dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main (){
+    int idade = 20; 
+    double altura = 1.85;
+    bool geek = (idade == altura);  
+    String frase = "Hello World"; 
+    
+    print(idade); 
+    print(altura);
+    print(geek);
+    print(frase);
+}
